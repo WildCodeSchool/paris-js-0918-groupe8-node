b690a4d04aa396b076e98406e8a99aab3140e0c1
Delphine BRUNET <delphine.brunet@gmail.com>
2018-12-05 13:04:36 +0100

    Update bios.js

--- routes/bios.js
+++ routes/bios.js
@@ -1,12 +1,9 @@
 const express = require('express');
-
 // const connection = require('../config/db');
-
 const Router = express.Router();
 
 Router.get('/', (req, res) => {
   res.send('Ceci est la route qui nous mène à /bios');
 });
-
 
 module.exports = Router;
