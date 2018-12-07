4961208895bf789bf933a12cd883653a819a8b5f
julielm77 <julielyminh@gmail.com>
2018-12-07 16:31:48 +0100

    route article get post put

--- routes/articles.js
+++ routes/articles.js
@@ -4,26 +4,19 @@ const connection = require('../config/db');
 
 const Router = express.Router();
 
-Router.post('/', (req, res) => {
-  res.send('Ceci est la route qui nous mène à /articles');
+
+Router.get('/', (req, res) => {
+  const sql = 'SELECT * FROM article';
+  connection.query(sql, (err, result) => {
+    if (err) throw err;
+    return res.status(200).send(result);
+  });
 });
 
-
-// Router.get('/showArticles', (req, res) => {
-//   const sql = 'SELECT * FROM conteurDigitalDB';
-//   connection.query(sql, (err, result) => {
-//     if (err) throw err;
-//     return res.status(200).send(result);
-//   });
-// });
-
-Router.post('/addArticles', (req, res) => {
+Router.post('/', (req, res) => {
   console.log('req BODY', req.body);
-  const sql = 'INSERT INTO conteurDigitalDB (id_article, create_date, update_date, title, content, blog_status, front_page_favorite ) VALUES (?,?,?,?,?,?,?)';
+  const sql = 'INSERT INTO article (id_article, create_date, update_date, title, content, blog_status, front_page_favorite) VALUES (null ,now() ,now() ,? ,? ,? ,? )';
   const values = [
-    req.body.id_article,
-    req.body.create_date,
-    req.body.update_date,
     req.body.title,
     req.body.content,
     req.body.blog_status,
@@ -35,18 +28,22 @@ Router.post('/addArticles', (req, res) => {
   });
 });
 
-// Router.put('/articles:id', (req, res) => {
-//   const idArticle = req.params.id;
-//   const formData = req.body;
-//   connection.query('UPDATE articles SET ? WHERE id = ?', [formData, idArticle], (err) => {
-//     if (err) {
-//       console.log(err);
-//       res.status(500).send('Erreur lors de la modification');
-//     } else {
-//       res.sendStatus(200);
-//     }
-//   });
-// });
+Router.put('/:id', (req, res) => {
+  console.log(req.body);
+  const idArticle = req.params.id;
+  const sql = ('UPDATE article SET update_date = now(), title = ?, content = ?, blog_status = ?, front_page_favorite = ? WHERE id_article = ?');
+  const values = [
+    req.body.title,
+    req.body.content,
+    req.body.blog_status,
+    req.body.front_page_favorite,
+    idArticle,
+  ];
+  connection.query(sql, values, (err, result) => {
+    if (err) throw err;
+    return res.status(200).send(result);
+  });
+});
 
 
 module.exports = Router;
