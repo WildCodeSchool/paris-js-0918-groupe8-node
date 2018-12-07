0b6bef3fb418806a219c0431c84f74bb9d322487
Af1ne <delphine.brunet@gmail.com>
2018-12-07 09:45:58 +0100

    adding get et post on commentaires

--- routes/commentaires.js
+++ routes/commentaires.js
@@ -18,22 +18,18 @@ Router.get('/', (req, res) => {
 // POST 1 nouveau commentaire (changer le port selon la config) :
 // Dans Postman
 // http://localhost:3001/api/commentaires/
-// Router.post('/', (req, res) => {
-//   // const formData = req.body;
-//   // console.log(formData);
-//   const sql = 'INSERT INTO comment SET (create_date, update_date, content, pseudo_anominous, mail, article_id_article) VALUES (?, ?, ?, ?, ?, ?)';
-//   const values = [
-//     req.body.create_date,
-//     req.body.update_date,
-//     req.body.content,
-//     req.body.pseudo_anominous,
-//     req.body.mail,
-//     req.body.article_id_article,
-//   ];
-//   connection.query(sql, values, (err, result) => {
-//     if (err) throw err;
-//     return res.status(200).send(result);
-//   });
-// });
+Router.post('/', (req, res) => {
+  const sql = 'INSERT INTO comment (id_comment, create_date, update_date, content, pseudo_anominous, mail, article_id_article) VALUES (null, now(), now(), ?, ?, ?, ?)';
+  const values = [
+    req.body.content,
+    req.body.pseudo_anominous,
+    req.body.mail,
+    req.body.article_id_article,
+  ];
+  connection.query(sql, values, (err, result) => {
+    if (err) throw err;
+    return res.status(200).send(result);
+  });
+});
 
 module.exports = Router;
