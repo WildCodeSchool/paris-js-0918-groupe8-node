f172f7f168f3ab0e42ded02a3733aa389c6f772e
Monica TARABUSI <code.mta@gmail.com>
2019-01-07 13:30:50 +0100

    ARTICLES - changement de port node : de 3000 à 3001 - regex : de chiffres à plusieurs chiffres - POST : ajout 2eme requete table de liaison admin_has_article - détails des champs à renseigner dans les routes front

--- routes/articles.js
+++ routes/articles.js
@@ -5,46 +5,46 @@ const connection = require('../config/db');
 const Router = express.Router();
 
 // Attention : changer le port selon la config :
-// http://localhost:3000/api/articles/
+// http://localhost:3001/api/articles/
 
-// localhost:3000/api/articles/
-// GET tous les articles :
-Router.get('/', (req, res) => {
-  const sql = 'SELECT * FROM article';
-  connection.query(sql, (err, result) => {
-    if (err) throw err;
-    return res.status(200).send(result);
-  });
-});
+// // localhost:3001/api/articles/
+// // GET tous les articles :
+// Router.get('/', (req, res) => {
+//   const sql = 'SELECT * FROM article';
+//   connection.query(sql, (err, result) => {
+//     if (err) throw err;
+//     return res.status(200).send(result);
+//   });
+// });
 
-// localhost:3000/api/articles/1
-// GET tous UN seul article par ID :
-// avec regex, limitant la saisie à des chiffres
-Router.get('/:id(\\d+)', (req, res) => {
+// // localhost:3001/api/articles/10
+// // GET UN seul article par ID :
+// // avec regex, limitant la saisie à des chiffres
+// Router.get('/:id(\\d{2,})', (req, res) => {
+//   const idArticle = req.params.id;
+//   const sql = ('SELECT * FROM article WHERE id_article = ?');
+//   connection.query(sql, idArticle, (err, result) => {
+//     if (err) throw err;
+//     return res.status(200).send(result);
+//   });
+// });
+
+
+// localhost:3001/api/articles/blog/10
+// GET de l'article du blog avec admin
+Router.get('/blog/:id(\\d{2,})', (req, res) => {
+  const sql = 'SELECT a.id_article, a.create_date, a.title, a.content, a.main_picture, ad.firstname, ad.lastname, ad.create_date, ad.avatar FROM article as a JOIN admin_has_article AS aa ON a.id_article = aa.article_id_article JOIN admin AS ad ON aa.admin_id_user = ad.id_user WHERE a.id_article = ?';
   const idArticle = req.params.id;
-  const sql = ('SELECT * FROM article WHERE id_article = ?');
   connection.query(sql, idArticle, (err, result) => {
     if (err) throw err;
     return res.status(200).send(result);
   });
 });
 
-
-// localhost:3000/api/articles/blog/1
-// GET de l'article avec admin
-Router.get('/blog/:id', (req, res) => {
-  const sql = 'SELECT a.id_article, a.create_date, a.title, a.content, aa.admin_id_user, aa.article_id_article, ad.id_user, ad.firstname, ad.lastname, ad.create_date, ad.avatar FROM article as a JOIN admin_has_article AS aa ON a.id_article = aa.article_id_article JOIN admin AS ad ON aa.admin_id_user = ad.id_user WHERE a.id_article = ?';
-  const idArticle = req.params.id;
-  connection.query(sql, idArticle, (err, result) => {
-    if (err) throw err;
-    return res.status(200).send(result);
-  });
-});
-
-// localhost:3000/api/articles/blog
+// localhost:3001/api/articles/blog
 // GET de tous les articles avec admin, id_article > 9
 Router.get('/blog', (req, res) => {
-  const sql = 'SELECT a.id_article, a.create_date, a.title, a.content, aa.admin_id_user, aa.article_id_article, ad.id_user, ad.firstname, ad.lastname, ad.create_date, ad.avatar FROM article as a JOIN admin_has_article AS aa ON a.id_article = aa.article_id_article JOIN admin AS ad ON aa.admin_id_user = ad.id_user WHERE a.id_article > 9';
+  const sql = 'SELECT a.id_article, a.create_date, a.title, a.content, a.main_picture, ad.firstname, ad.lastname, ad.create_date, ad.avatar FROM article as a JOIN admin_has_article AS aa ON a.id_article = aa.article_id_article JOIN admin AS ad ON aa.admin_id_user = ad.id_user WHERE a.id_article > 9';
   connection.query(sql, (err, result) => {
     if (err) throw err;
     return res.status(200).send(result);
@@ -63,10 +63,10 @@ Router.get('/blog', (req, res) => {
 // });
 
 
-// localhost:3000/api/articles/accroche
+// localhost:3001/api/articles/accroche
 // GET accueil : accroche :
 Router.get('/accroche', (req, res) => {
-  const sql = 'SELECT title, content, main_picture FROM article WHERE id_article = 1';
+  const sql = 'SELECT title, content FROM article WHERE id_article = 1';
   connection.query(sql, (err, result) => {
     if (err) throw err;
     return res.status(200).send(result);
@@ -74,7 +74,7 @@ Router.get('/accroche', (req, res) => {
 });
 
 
-// localhost:3000/api/articles/charte-short
+// localhost:3001/api/articles/charte-short
 // GET accueil : charte courte
 Router.get('/charte-short', (req, res) => {
   const sql = 'SELECT title, short_content FROM article WHERE id_article = 2';
@@ -85,7 +85,7 @@ Router.get('/charte-short', (req, res) => {
 });
 
 
-// localhost:3000/api/articles/charte-longue
+// localhost:3001/api/articles/charte-long
 // GET page charte
 Router.get('/charte-long', (req, res) => {
   const sql = 'SELECT title, content, main_picture FROM article WHERE id_article = 3';
@@ -96,28 +96,25 @@ Router.get('/charte-long', (req, res) => {
 });
 
 
-// localhost:3000/api/articles/newsletter
+// localhost:3001/api/articles/newsletter
 // GET accueil : newsletter
 Router.get('/newsletter', (req, res) => {
-  const sql = 'SELECT title, content, main_picture FROM article WHERE id_article = 4';
+  const sql = 'SELECT title, content FROM article WHERE id_article = 4';
   connection.query(sql, (err, result) => {
     if (err) throw err;
     return res.status(200).send(result);
   });
 });
 
-// localhost:3000/api/articles/services
+// localhost:3001/api/articles/services
 // GET accueil : services
-Router.get('/services1', (req, res) => {
+Router.get('/services', (req, res) => {
   const sql = 'SELECT title, short_content, main_picture FROM article WHERE id_article BETWEEN 5 AND 7';
   connection.query(sql, (err, result) => {
     if (err) throw err;
     return res.status(200).send(result);
   });
 });
-
-
-
 
 
 // // GET id_article SI medias lié
@@ -131,30 +128,30 @@ Router.get('/services1', (req, res) => {
 //   });
 // });
 
-// localhost:3000/api/articles/1/commentaires
-// GET id_article SI commentaire lié
-Router.get('/:id/commentaires', (req, res) => {
-  const sql = 'SELECT * FROM `comment` WHERE article_id_article = ?';
-  const idArticle = req.params.id;
-  connection.query(sql, idArticle, (err, result) => {
-    if (err) throw err;
-    return res.status(200).send(result);
-  });
-});
 
-// localhost:3000/api/articles/
+// localhost:3001/api/articles/
+// champs à renseigner :
+// title, content, main_picture, blog_status, front_page_favorite,
+// >>>>>   + OBLIGATOIRE : admin_id_user !!   <<<<<<
 // POST 1 nouvel article :
 Router.post('/', (req, res) => {
-  const sql = 'INSERT INTO article (title, content, blog_status, front_page_favorite) VALUES (? ,? ,? ,? )';
-  const values = [
+  const sql = 'INSERT INTO article (title, content, main_picture, blog_status, front_page_favorite) VALUES (? ,? ,? ,?, ?)';
+  const valuesArticle = [
     req.body.title,
     req.body.content,
+    req.body.main_picture,
     req.body.blog_status,
     req.body.front_page_favorite,
   ];
-  connection.query(sql, values, (err, result) => {
+  connection.query(sql, valuesArticle, (err, result) => {
     if (err) throw err;
-    return res.status(200).send(result);
+    const sqlRelation = 'INSERT INTO admin_has_article (admin_id_user, article_id_article) VALUES (?, LAST_INSERT_ID())';
+    const idAdmin = req.body.admin_id_user;
+    connection.query(sqlRelation, idAdmin, (error, resultRelation) => {
+      if (error) throw error;
+      return res.send(resultRelation);
+    });
+    // return res.status(200).send(result);
   });
 });
 
@@ -162,10 +159,12 @@ Router.post('/', (req, res) => {
 // attention : impossible de détailler, dans le PUT, les champs à modifier
 // sinon, ceux pas modifiés sont écrasés
 
-// localhost:3000/api/articles/1
-// PUT modification d'un contenu d'un article
+// localhost:3001/api/articles/blog/10
+// champs à prévoir :
+// title, content, main_picture, blog_status, front_page_favorite
+// PUT modification d'un contenu d'un article du blog
 // avec regex, limitant la saisie à des chiffres
-Router.put('/:id(\\d{2,})', (req, res) => {
+Router.put('/blog/:id(\\d{2,})', (req, res) => {
   const sql = ('UPDATE article SET ? WHERE id_article = ?');
   const formData = req.body;
   const idArticle = req.params.id;
@@ -175,7 +174,8 @@ Router.put('/:id(\\d{2,})', (req, res) => {
   });
 });
 
-// localhost:3000/api/articles/accroche
+// localhost:3001/api/articles/accroche
+// >>> Il faut, dans le front, donner accès seulement au champs 'title' + 'content'
 // PUT modification accueil, accroche
 Router.put('/accroche', (req, res) => {
   const sql = ('UPDATE article SET ? WHERE id_article = 1');
@@ -186,7 +186,7 @@ Router.put('/accroche', (req, res) => {
   });
 });
 
-// localhost:3000/api/articles/charte-short
+// localhost:3001/api/articles/charte-short
 // >>> Il faut, dans le front, donner accès seulement au champs 'title' + 'short_content'
 // PUT modification accueil, charte courte
 Router.put('/charte-short', (req, res) => {
@@ -198,7 +198,7 @@ Router.put('/charte-short', (req, res) => {
   });
 });
 
-// localhost:3000/api/articles/charte-long
+// localhost:3001/api/articles/charte-long
 // >>> Il faut, dans le front, donner accès seulement au champs 'title' + 'content' + 'main_picture'
 // PUT modification page charte
 Router.put('/charte-long', (req, res) => {
@@ -210,7 +210,7 @@ Router.put('/charte-long', (req, res) => {
   });
 });
 
-// localhost:3000/api/articles/newsletter
+// localhost:3001/api/articles/newsletter
 // >>> Il faut, dans le front, donner accès seulement aux champs
 // >>> 'title' + 'short_content' + 'main_picture'
 // PUT modification accueil newsletter
@@ -227,7 +227,7 @@ Router.put('/newsletter', (req, res) => {
 // >>> 'title' + 'short_content' + 'main_picture'
 // PUT modification accueil 3 services, mais un à la fois
 
-// localhost:3000/api/articles/service1
+// localhost:3001/api/articles/service1
 Router.put('/service1', (req, res) => {
   const sql = ('UPDATE article SET ? WHERE id_article = 5');
   const formData = req.body;
@@ -236,7 +236,7 @@ Router.put('/service1', (req, res) => {
     return res.status(200).send(result);
   });
 });
-// localhost:3000/api/articles/service2
+// localhost:3001/api/articles/service2
 Router.put('/service2', (req, res) => {
   const sql = ('UPDATE article SET ? WHERE id_article = 6');
   const formData = req.body;
@@ -245,7 +245,7 @@ Router.put('/service2', (req, res) => {
     return res.status(200).send(result);
   });
 });
-// localhost:3000/api/articles/service3
+// localhost:3001/api/articles/service3
 Router.put('/service3', (req, res) => {
   const sql = ('UPDATE article SET ? WHERE id_article = 7');
   const formData = req.body;
@@ -255,30 +255,20 @@ Router.put('/service3', (req, res) => {
   });
 });
 
-// >>> Il faut, dans le front, donner accès seulement aux champs
-// >>> 'title' + 'content' + 'main_picture'
-// PUT modification blog, mais un à la fois
-// localhost:3000/api/articles/blog/:id
-Router.put('/blog/:id(\\d{2,})', (req, res) => {
-  const sql = ('UPDATE article SET ? WHERE id_article = 7');
-  const formData = req.body;
-  connection.query(sql, formData, (err, result) => {
-    if (err) throw err;
-    return res.status(200).send(result);
-  });
-});
+// ******************************************
+// >>> la route DELETE NE FONCTIONNE PLUS,
+// >>> à cause de la table de liaison !!!
 
-
-// localhost:3000/api/articles/11
-// DELETE effacer un article selon d'id:
-Router.delete('/:id', (req, res) => {
-  const idArticle = req.params.id;
-  const sql = ('DELETE FROM article WHERE id_Article = ?');
-
-  connection.query(sql, idArticle, (err) => {
-    if (err) throw err;
-    return res.status(200).end();
-  });
-});
+// // localhost:3001/api/articles/blog/21
+// // DELETE effacer un article selon d'id:
+// Router.delete('blog/:id', (req, res) => {
+//   const sql = 'DELETE FROM article WHERE id_Article = ?';
+//   const idArticle = req.params.id;
+//   connection.query(sql, idArticle, (err) => {
+//     if (err) throw err;
+//     return res.status(200).end();
+//   });
+// });
+// ********************************************
 
 module.exports = Router;
